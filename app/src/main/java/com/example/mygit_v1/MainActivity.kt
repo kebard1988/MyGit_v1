b0eb0f8a55841100ac4b88e9007df837f2cd42e7
kebard1988 <kebard1988@gmail.com>
2023-01-04 22:28:01 +0300

    включена actyvy_biding_fix

--- app/src/main/java/com/example/mygit_v1/MainActivity.kt
+++ app/src/main/java/com/example/mygit_v1/MainActivity.kt
@@ -2,9 +2,14 @@ package com.example.mygit_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mygit_v1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindinviv : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        bindinviv = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindinviv.root)
     }
 }
